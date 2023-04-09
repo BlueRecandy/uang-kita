4e1b40fa8c548152fa840038108b3a85a3ebdd5f
luttfimohamad <luttfimohamad@gmail.com>
2023-04-10 01:04:57 +0700

    add BackgroundContainer

--- lib/screens/tambah_pengeluaran_screen.dart
+++ lib/screens/tambah_pengeluaran_screen.dart
@@ -12,16 +12,60 @@ class _TambahPengeluaranScreenState extends State<TambahPengeluaranScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tambah Pengeluaran'),
-      ),
       body: SafeArea(
-        child: Column(
-          children: [
-            const Text('Tambah Pengeluaran'),
-          ],
+        child: Stack(
+          children: [BackgroundContainer(context)],
         ),
       ),
+    );
+  }
+
+  Column BackgroundContainer(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 240,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 40),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Icon(Icons.arrow_back, color: Colors.white),
+                    ),
+                    Text(
+                      'Tambah Pengeluaran',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                    Icon(
+                      Icons.attach_file_outlined,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
