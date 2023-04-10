bc2d5e4f63fb2536b5f1dae0a66e83a686937022
luttfimohamad <luttfimohamad@gmail.com>
2023-04-10 19:36:08 +0700

    add const

--- lib/widgets/screens/tambah pengeluaran/textfield_judul.dart
+++ lib/widgets/screens/tambah pengeluaran/textfield_judul.dart
@@ -18,16 +18,17 @@ class JudulTextField extends StatelessWidget {
         focusNode: focusNode,
         controller: controller,
         decoration: InputDecoration(
-          contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
           labelText: 'Judul',
           labelStyle: TextStyle(fontSize: 17, color: Colors.grey.shade500),
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(width: 2, color: Color(0xffC5C5C5)),
+            borderSide: const BorderSide(width: 2, color: Color(0xffC5C5C5)),
           ),
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(width: 2, color: Color(0xff368983)),
+            borderSide: const BorderSide(width: 2, color: Color(0xff368983)),
           ),
         ),
       ),
