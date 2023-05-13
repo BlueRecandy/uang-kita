3c37858cb4adeb056adf0c8a20c7d40c39bdc638
TiveCS <ahmadfathanhanif@gmail.com>
2023-05-13 07:38:01 +0700

    add utility convert map to bill

--- lib/utils/model_utils.dart
+++ lib/utils/model_utils.dart
@@ -1,3 +1,4 @@
+import 'package:uang_kita/models/bill_model.dart';
 import 'package:uang_kita/models/expense_model.dart';
 
 import '../models/category_type_model.dart';
@@ -13,5 +14,18 @@ class ModelUtils {
         category: category!);
 
     return expense;
+  }
+
+  static Bill mapToBill(Map<String, dynamic> map) {
+    final category = categoryTypeMap[map['category'].toString()];
+
+    final bill = Bill(
+        id: map['id'] as int,
+        title: map['title'] as String,
+        amount: int.parse(map['amount'].toString()),
+        dueDate: DateTime.parse(map['due_date'].toString()),
+        category: category!);
+
+    return bill;
   }
 }
