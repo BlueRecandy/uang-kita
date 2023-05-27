0ea8a63b5bc1a08e6f38cdc06be8be96dd984585
TiveCS <ahmadfathanhanif@gmail.com>
2023-05-27 14:21:39 +0700

    implement bill repository

--- lib/repositories/bill_repository.dart
+++ lib/repositories/bill_repository.dart
@@ -1,10 +1,10 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:uang_kita/models/bill_model.dart';
 import 'package:uang_kita/models/category_type_model.dart';
-import 'package:uang_kita/repositories/interfaces/interface_repository.dart';
+import 'package:uang_kita/repositories/interfaces/interface_bill_repository.dart';
 import 'package:uang_kita/utils/model_utils.dart';
 
-class BillRepository extends IRepository<Bill, int> {
+class BillRepository extends IBillRepository {
   static const tableName = 'bills';
 
   @override
@@ -26,24 +26,6 @@ class BillRepository extends IRepository<Bill, int> {
   }
 
   @override
-  Future<Bill?> findById(Database db, int id) {
-    return db.query(tableName, where: 'id = ?', whereArgs: [id]).then((record) {
-      if (record.isNotEmpty) {
-        return ModelUtils.mapToBill(record.first);
-      } else {
-        return null;
-      }
-    });
-  }
-
-  @override
-  Future<List<Bill>> findMany(Database db) {
-    return db.query(tableName, orderBy: 'date DESC').then((records) {
-      return records.map((record) => ModelUtils.mapToBill(record)).toList();
-    });
-  }
-
-  @override
   Future<void> insert(Database db, Map<String, dynamic> data) {
     // validate data
     if (data['title'] == null ||
@@ -58,5 +40,22 @@ class BillRepository extends IRepository<Bill, int> {
     data['category'] = category.name;
 
     return db.insert(tableName, data);
+  }
+
+  @override
+  Future<void> update(Database db, int id, Map<String, dynamic> data) {
+    // validate data
+    if (data['title'] == null ||
+        data['category'] == null ||
+        data['amount'] == null ||
+        data['due_date'] == null) {
+      throw Exception('Field title, category, amount, dan due_date kosong');
+    }
+
+    final category = data['category'] as CategoryType;
+
+    data['category'] = category.name;
+
+    return db.update(tableName, data, where: 'id = ?', whereArgs: [id]);
   }
 }
